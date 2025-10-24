efbc9c27e348e2f939c1dc5ef31bca0bd23dbc75
keli281 <awinjanatalie77@gmail.com>
2025-10-24 05:26:31 +0300

    fix case sensitivity

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Navbar from './components/navbar';
-import Footer from './components/footer';
-import Home from './pages/home';
-import Tasks from './pages/tasks';
-import About from './pages/about';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Tasks from './pages/Tasks';
+import About from './pages/About';
 
 function App() {
   return (
